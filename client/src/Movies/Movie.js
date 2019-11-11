9f2ddc968e9f53e837959ac93c403a729a65dc8e
April Ding <aprilding@Aprils-iMac.local>
2019-11-11 18:01:47 -0500

    week8day1Projects

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -3,12 +3,13 @@ import axios from 'axios';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
- 
+  console.log(props);
+  
   useEffect(() => {
-    const id = 1;
+    const id=props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
-
+    const getMovie = () => {
        axios
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
@@ -16,8 +17,8 @@ const Movie = (props) => {
         })
         .catch(error => {
           console.error(error);
-        });
-
+        });}
+      getMovie();
   },[]);
   
   // Uncomment this only when you have moved on to the stretch goals
